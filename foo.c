4b8476dde2ff7f84c583404485c5df33f5c1f8f5
Carinapremi67 <2carina.prem@gmail.com>
2025-12-18 12:05:12 +0000

    Test

--- foo.c
+++ foo.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
+
+ // Aufgabe 1: Weihnachtskekse
+
+ int number_biscuits = 0;
+ printf("Gib die Anzahl der Kekse an: \n");
+scanf("%d", number_biscuits);
+
+ float diameter_biscuit = 0;
+ printf("Gib den Durchmesser der Kekse an: \n");
+ scanf("%lf", diameter_biscuit);
+
+
+
+
+//Augabe 2: Christbaumschmuck
+
+int candles = 0;
+int balls = 0;
+printf("Gib die Anzahl der Kerzen ein: \n");
+scanf("%d", candles);
+printf("Gib die Anzahl der Kugeln  ein: \n");
+scanf("%d", balls);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
